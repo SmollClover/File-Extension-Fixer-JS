1f71bb1ee1b922b7b30d78caaeb88a1499db6e83
Clover <belitzlars@gmail.com>
2026-03-05 19:45:43 +0100

    feat: compile to executable instead of script

--- build.ts
+++ build.ts
@@ -1,12 +1,33 @@
+import { type } from 'node:os';
+import type { Build } from 'bun';
+
+let target: Build.CompileTarget;
+switch (type()) {
+    case 'Linux':
+        target = 'bun-linux-x64-modern';
+        break;
+    case 'Darwin':
+        target = 'bun-darwin-x64-modern';
+        break;
+    case 'Windows_NT':
+        target = 'bun-windows-x64-modern';
+        break;
+    default:
+        throw Error('Build target could not be determined');
+}
+
 console.log(
     (
         await Bun.build({
             entrypoints: ['src/index.ts'],
             outdir: './out',
-            minify: true,
             target: 'bun',
+            minify: true,
             sourcemap: 'inline',
-            naming: 'fixFileExt.js',
+            compile: {
+                target,
+                outfile: 'fixFileExt',
+            },
         })
     ).logs.join('\n'),
 );
